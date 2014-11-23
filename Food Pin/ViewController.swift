0a7efaccef29cd1c149e8dd718525df71d797950
Jinma li <lijinma@126.com>
2014-11-23 19:06:40 +0800

    - complete section: 5. Creating a Simple Table Based App

--- Food Pin/ViewController.swift
+++ Food Pin/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh’s Chocolate", "Palomino Espresso","Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle &Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // Return the number of rows in the section.
+            return restaurantNames.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->
+            UITableViewCell {
+            let cellIdentifier = "Cell"
+            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as UITableViewCell
+            // Configure the cell...
+            cell.textLabel.text = restaurantNames[indexPath.row]
+            cell.imageView.image = UIImage(named: "restaurant")
+            return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +34,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
 
