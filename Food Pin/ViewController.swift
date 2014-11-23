15b6c2ed1d70c4968b1a0bb596c8a377d1bb8043
Jinma li <lijinma@126.com>
2014-11-23 18:05:42 +0800

    Initial Commit

---
+++ Food Pin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Pin
+//
+//  Created by Li JinMa on 14/11/23.
+//  Copyright (c) 2014年 Li JinMa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
