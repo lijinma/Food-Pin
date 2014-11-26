bf4c2b0afaa4b2af4c18f4abadde62892b24cabb
Jinma li <lijinma@126.com>
2014-11-26 10:16:29 +0800

    - Complete 7: Table Cell Selection and UIAlertController

--- Food Pin/RestaurantTableViewController.swift
+++ Food Pin/RestaurantTableViewController.swift
@@ -13,10 +13,12 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh’s Chocolate", "Palomino Espresso","Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle &Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-
+    
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish","Spanish", "British", "Thai"]
     
     var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "thaicafe.jpg"]
+    
+    var restaurantIsVisited = [Bool](count: 21, repeatedValue: false)
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
@@ -33,6 +35,8 @@ class RestaurantTableViewController: UITableViewController {
             cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
             cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.size.width / 2
             cell.thumbnailImageView.clipsToBounds = true
+            
+            cell.accessoryType = restaurantIsVisited[indexPath.row] ? .Checkmark : .None
             return cell
     }
     
@@ -58,15 +62,35 @@ class RestaurantTableViewController: UITableViewController {
         // Return the number of sections.
         return 1
     }
-
+    
     override func tableView(tableview: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    let optionMenu = UIAlertController(title: nil, message: "What do you want to do?",
-        preferredStyle: .ActionSheet)
-    // Add actions to the menu
-    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-    optionMenu.addAction(cancelAction)
-    // Display the menu
-    self.presentViewController(optionMenu, animated: true, completion: nil)
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?",
+            preferredStyle: .ActionSheet)
+        // Add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+        // Display the menu
+        
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .Alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alertMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style:
+            UIAlertActionStyle.Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        let isVisitedAction = UIAlertAction(title: "I've been here", style: .Default, handler: {
+            (action:UIAlertAction!) -> Void in
+            let cell = self.tableView.cellForRowAtIndexPath(indexPath)
+            self.restaurantIsVisited[indexPath.row] = ~self.restaurantIsVisited[indexPath.row]
+            cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .Checkmark : .None
+            })
+        optionMenu.addAction(isVisitedAction)
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+        
     }
     
     /*
