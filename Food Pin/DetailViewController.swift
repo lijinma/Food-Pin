2b26d9398fee25a8571fd34f71eeaf148782e40f
Jinma li <lijinma@126.com>
2014-11-27 13:55:53 +0800

    - complete 12: Self Sizing Cells

--- Food Pin/DetailViewController.swift
+++ Food Pin/DetailViewController.swift
@@ -24,6 +24,9 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
             CGRectZero)
         self.tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0,alpha: 0.8)
         title = self.restaurant.name
+        
+        tableView.estimatedRowHeight = 36.0;
+        tableView.rowHeight = UITableViewAutomaticDimension;
     }
 
     override func viewWillAppear(animated: Bool) {
